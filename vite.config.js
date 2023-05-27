1b61174f1325f6e7855e8bc7e370f4e14ef8f5fc
xmaycol.2020.5@gmail.com <xmaycol.2020.@5@gmail.com>
2023-05-27 00:48:37 -0500

    change http/base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-   https: 'https://Maycolx.github.io/Average_calculator',
+   base: 'https://Maycolx.github.io/Average_calculator',
 });
